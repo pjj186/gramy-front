949ee1715053c17b20601bd5425781294b058033
Jong jae Park <pjj186@naver.com>
2022-04-29 19:39:11 +0900

    구매 문의 게시판 상세보기 틀 작성

--- src/routes/PurchaseDetail.jsx
+++ src/routes/PurchaseDetail.jsx
@@ -1,7 +1,28 @@
-import Recat from "react";
+import React from "react";
+import Helmet from "react-helmet";
+import BoardTitle from "../components/BoardTitle";
+import { useLocation } from "react-router-dom";
 
 const PurchaseDetail = () => {
-  return <div>구매 문의 상세 페이지</div>;
+  const location = useLocation();
+  console.log(location);
+  return (
+    <>
+      <Helmet>
+        <title>GRAMy | {`${location.state.info.name}`}</title>
+      </Helmet>
+      <div className="w-full h-full">
+        <BoardTitle title="구매 문의 게시판" />
+        <div className="w-full h-full bg-orange-300">
+          {/* 게시글 번호, 제목 */}
+          <div className="w-full h-full flex justify-center items-center">
+            <span>번호</span>
+            <span>제목</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PurchaseDetail;
