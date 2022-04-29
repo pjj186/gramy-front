6c84cdb074b6d0d2413ce1d65ba4c3d78d3be267
Jong jae Park <pjj186@naver.com>
2022-04-29 19:37:24 +0900

    글쓰기 페이지 틀 생성

--- src/routes/PurchaseWrite.jsx
+++ src/routes/PurchaseWrite.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Helmet from "react-helmet";
+import BoardTitle from "../components/BoardTitle";
 
 const PurchaseWrite = () => {
-  return <div>구매 문의 글쓰기 페이지</div>;
+  return (
+    <>
+      <Helmet>
+        <title>GRAMy | 구매문의 작성</title>
+      </Helmet>
+      <div className="w-full h-full">
+        <BoardTitle title="구매 문의 작성" />
+      </div>
+    </>
+  );
 };
 
 export default PurchaseWrite;
